acf74a54fe2a38d0a6bacb6dbb62dc1ef99ee74c
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-14 12:54:08 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,6 +1,15 @@
-require 'yaml'
+require 'pp'
+
 
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+pp directors_database
+hash = {
+  "1st Director's Name"=>1234567890,
+  "2nd Director's Name"=>1234577890,
+  "3rd Director's Name"=>1234709136,
+  ...
+}
